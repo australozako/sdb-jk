b8c920ba104d90f15132fff8f227b61bc8c3c62a
Zako SD <zakofr12@gmail.com>
2024-03-02 12:04:25 +0100

    trjk ...

---
+++ src/main/java/fr/handsonjk/sdbjk/SdbJkApplication.java
@@ -0,0 +1,13 @@
+package fr.handsonjk.sdbjk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdbJkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdbJkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/handsonjk/sdbjk/SdbJkApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.handsonjk.sdbjk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdbJkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
